fb6328eb1ff7f6f50c600c797e75a17872ab405e
Sam Hart <criswellious@gmail.com>
2016-01-25 14:04:51 -0500

    more work on error stats, require requests

--- error-stats.py
+++ error-stats.py
@@ -4,9 +4,12 @@
 # an analysis of the errors.
 
 from __future__ import print_function
+import sys
+import subprocess
 import json
 import argparse
 import hashlib
+import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from data_models import Base, Job, Error
@@ -31,6 +34,12 @@ engine = create_engine(config['database-url'])
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
+BASE_API_URL = 'https://circleci.com/api/v1'
+
+def get_cmd_output(command):
+    output = subprocess.Popen(command.split(),
+            stdout=subprocess.PIPE).communicate()[0].decode('ascii')
+    return output.strip()
 
 def log(line):
     if args.verbose:
@@ -42,10 +51,18 @@ for repo in config['repos']:
         print(repo)
         sys.exit(1)
 
+    log("> Processing '{0}'".format(repo['path']))
+
     hash_id = hashlib.md5(repo['path']).hexdigest()
 
     q = session.query(Job).filter(Job.repo_hash == hash_id)
     q = q.filter(Job.outcome == 'failed')
     results = q.all()
+    circle_user = get_cmd_output('git -C {0} config git-circle.user'.format(
+        repo['path']))
+    circle_project = get_cmd_output(
+            'git -C {0} config git-circle.project'.format(repo['path']))
+    log(">--> user: {0}, project: {1}".format(circle_user, circle_project))
+
     for result in results:
         print(result.build_number)
