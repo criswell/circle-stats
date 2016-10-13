f7282264f847085788786d9108e535b5e6b6cf7b
Sam Hart <hartsn@gmail.com>
2016-10-13 08:20:29 -0400

    test query

--- status_stats.py
+++ status_stats.py
@@ -7,7 +7,7 @@ import argparse
 import hashlib
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from data_models import Base, Job, Error
+from data_models import Base, Job
 
 parser = argparse.ArgumentParser()
 parser.add_argument("config", help="The JSON config file to load.")
@@ -34,4 +34,22 @@ def log(line):
     if args.verbose:
         print(line)
 
+for repo in config['repos']:
+    if not repo.has_key('path'):
+        print("A repo entry is missing needed keys!")
+        print(repo)
+        sys.exit(1)
 
+    log("> Processing '{0}'".format(repo['path']))
+
+    if repo.has_key('hash'):
+        hash_id = repo['hash']
+    else:
+        hash_id = hashlib.md5(repo['path']).hexdigest()
+
+    # First, obtain all the possible statuses from the db
+    q = session.query(Job).filter(Job.repo_hash == hash_id)
+    q = q.distinct(Job.status)
+    results = q.all()
+
+    print(results)
